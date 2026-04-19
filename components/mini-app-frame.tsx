cc16a1b4faa4b6822ca8f015de6758ea22a6de70
TheTevea <chhemtevea@gmail.com>
2026-04-20 00:01:03 +0700

    remove header

--- components/mini-app-frame.tsx
+++ components/mini-app-frame.tsx
@@ -6,7 +6,7 @@ import { PageTransition } from "@/components/page-transition"
 export function MiniAppFrame({ children }: { children: ReactNode }) {
   return (
     <div className="relative mx-auto flex min-h-[100dvh] w-full max-w-[440px] flex-col bg-background">
-      <AppHeader />
+      {/* <AppHeader /> */}
       <main className="flex-1 px-4 pb-[120px] pt-2">
         <PageTransition>{children}</PageTransition>
       </main>
